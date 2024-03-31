3d823fc1b97b2ba79132b7e4d7c1cc5291baa4d4
AdilCodeBX <jihadmdna@gmail.com>
2024-03-31 15:44:54 +0200

    add changes

--- src/init.js
+++ src/init.js
@@ -1,1 +1,17 @@
-alert('init.js loaded!');
+const count = document.getElementById('count');
+const decrease = document.getElementById('decrease');
+const increase = document.getElementById('increase');
+
+const reset = document.getElementById('reset');
+
+increase.addEventListener('click', () => {
+  count.innerHTML++;
+});
+
+decrease.addEventListener('click', () => {
+  count.innerHTML--;
+});
+
+reset.addEventListener('click', () => {
+  count.innerHTML = 0;
+});
